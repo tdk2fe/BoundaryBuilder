80b15771c96b79bcfbd973bb7295c31c14460a06
Tim krupinski <tdkrupinski@gmail.com>
2013-10-23 20:54:36 -0500

    refactored javascript and css into seperate files

--- js/gmaps.js
+++ js/gmaps.js
@@ -1,128 +1,128 @@
-/**
- * Created by Tim on 10/23/13.
- */
-
 var poly;
 var map;
+
 var listeners = [
-    'mapclicks',
-    'markerclicks'
+	'mapclicks',
+	'markerclicks'
 ];
+
 var markers = [];
 
 function initialize() {
-    google.maps.visualRefresh = true;
+	google.maps.visualRefresh = true;
 
-    var mapOptions = {
-        center: new google.maps.LatLng(38,-92),
-        zoom: 8,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+	var mapOptions = {
+		center: new google.maps.LatLng(38,-92),
+		zoom: 8,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
 
-    map = new google.maps.Map(document.getElementById("map-canvas"),
-        mapOptions);
+	map = new google.maps.Map(document.getElementById("map-canvas"),
+		mapOptions);
 
-    var lineSymbol = {
-        path: 'M 0,-1 0,1',
-        strokeOpacity: 1,
-        strokeColor: '#E94100',
-        strokeWeight: 1.2,
-        scale: 3
-    };
+	var lineSymbol = {
+		path: 'M 0,-1 0,1',
+		strokeOpacity: 1,
+		strokeColor: '#E94100',
+		strokeWeight: 1.2,
+		scale: 3
+	};
 
-    var polyOptions = {
-        strokeColor: '#000000',
-        strokeOpacity: 0,
-        strokeWeight: .5,
-        icons: [{
-            icon: lineSymbol,
-            offset: '0',
-            repeat: '10px'
-        }]
-    };
+	var polyOptions = {
+		strokeColor: '#000000',
+		strokeOpacity: 0,
+		strokeWeight: .5,
+		icons: [{
+			icon: lineSymbol,
+			offset: '0',
+			repeat: '10px'
+		}]
+	};
 
-    poly = new google.maps.Polyline(polyOptions);
-    poly.setMap(map);
+	poly = new google.maps.Polyline(polyOptions);
+	poly.setMap(map);
 
-    listeners['mapclicks'] = google.maps.event.addListener(map, 'click', addLatLng);
+	listeners['mapclicks'] = google.maps.event.addListener(map, 'click', addLatLng);
 
-    document.getElementById("clearmap").disabled = true;
+	document.getElementById("clearmap").disabled = true;
 }
 
 function addLatLng(event) {
-    var path = poly.getPath();
+	var path = poly.getPath();
 
-    path.push(event.latLng);
+	path.push(event.latLng);
 
-    var encoded_path = google.maps.geometry.encoding.encodePath(path);
+	var image = {
+		url: 'images/marker.png',
+		origin: new google.maps.Point(0,0),
+		anchor: new google.maps.Point(6,6)
+	}
 
+	var marker = new google.maps.Marker({
+		position: event.latLng,
+		title: '#' + path.getLength(),
+		map: map,
+		icon: image
+	});
 
-    var image = {
-        url: 'images/marker.png',
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(6, 6)
-    }
+	markers.push(marker);
+	if (markers.length == 1) {
+		listeners['markerclicks'] = google.maps.event.addListener(marker, 'click', closeBoundary);
+	}
 
-    var marker = new google.maps.Marker({
-        position: event.latLng,
-        title: '#' + path.getLength(),
-        map: map,
-        icon: image
-    });
+	var latcord = event.latLng.lat()
+	latcord = latcord.toString().slice(0,7);
 
-    markers.push(marker);
-    if (markers.length == 1) {
-        listeners['markerclicks'] = google.maps.event.addListener(marker, 'click', closeBoundary);
-    }
+	var longcord = event.latLng.lng();
+	longcord = longcord.toString().slice(0,7);
 
-    var latcord = event.latLng.lat()
-    latcord = latcord.toString().slice(0,7);
+	var coordlist = document.getElementById("list");
 
-    var longcord = event.latLng.lng();
-    longcord = longcord.toString().slice(0,7);
+	coordlist.innerHTML += "<li>" + latcord + ", " + longcord + "</li>"
 
-    var coordlist = document.getElementById("list");
+	var resetButton = document.getElementById("clearmap")
+	resetButton.disabled = false;
 
-    coordlist.innerHTML += "<li>" + latcord + ", " + longcord + "</li>"
+    var encoded = google.maps.geometry.encoding.encodePath(path);
+    var encodedstring = document.getElementById("encodedstring");
 
-    var resetButton = document.getElementById("clearmap")
-    resetButton.disabled = false;
+    encodedstring.innerHTML = encoded;
 
-    var encodedstring = document.getElementById("region-encoded");
-    encodedstring.innerHTML += encoded_path;
 
 }
 
 function closeBoundary() {
-    var path = poly.getPath();
-    path.push(markers[0].position);
-    google.maps.event.removeListener(listeners['mapclicks']);
+	var path = poly.getPath();
+	path.push(markers[0].position);
+	google.maps.event.removeListener(listeners['mapclicks']);
 }
 
 function setAllMap(map) {
-    for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(map);
-    }
+	for (var i = 0; i < markers.length; i++) {
+		markers[i].setMap(map);
+	}
 }
 
 function clearMarkers() {
 
-    setAllMap(null);
+	setAllMap(null);
 }
 
 function reset() {
-    var path = poly.getPath();
-    path.clear();
-    clearMarkers();
-    markers = [];
+	var path = poly.getPath();
+	path.clear();
+	clearMarkers();
+	markers = [];
 
-    listeners['mapclicks'] = google.maps.event.addListener(map, 'click', addLatLng);
+	listeners['mapclicks'] = google.maps.event.addListener(map, 'click', addLatLng);
 
-    var coordlist = document.getElementById("list");
-    coordlist.innerHTML = "";
+	var coordlist = document.getElementById("list");
+	coordlist.innerHTML = "";
 
-    var resetButton = document.getElementById("clearmap")
-    resetButton.disabled = true;
+	var resetButton = document.getElementById("clearmap")
+	resetButton.disabled = true;
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
